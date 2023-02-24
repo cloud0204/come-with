09d4f5bb9ab040ad518106e715053e9928e5e8e6
cloud <you@example.com>
2023-02-24 16:34:27 +0800

    feat: add Post & Comment

--- src/components/models.ts
+++ src/components/models.ts
@@ -6,3 +6,27 @@ export interface Todo {
 export interface Meta {
   totalCount: number;
 }
+
+export interface Post {
+  commentCount: number;
+  id: number;
+  likes: number;
+  slug: string;
+  status: string;
+  summary: string;
+  tags: [{ slug: string }];
+  thumbnail: string;
+  title: string;
+  topPriority: number;
+  updateTime: number;
+  visits: number;
+}
+
+export interface Comment {
+  author: string;
+  authorUrl?: string;
+  content: string;
+  email: string;
+  postId: number;
+  parentId?: number;
+}
